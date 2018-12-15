07f6b68258d0daee1a7a958da0bb362e8facbbb5
mahdi <mahdihp.devsc@gmail.com>
2018-12-15 09:24:23 +0330

    Merge branch 'master' of /home/mahdi/IdeaProjects/mostashar with conflicts.

--- src/main/java/ir/mostashar/model/AuditModel.java
+++ src/main/java/ir/mostashar/model/AuditModel.java
@@ -1,7 +1,10 @@
 package ir.mostashar.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
@@ -29,6 +32,16 @@ public abstract class AuditModel implements Serializable {
     @Column(name = "updated_at", nullable = false)
     @LastModifiedDate
     private Date updatedAt;
+
+//    @CreatedBy
+//    @Column(name = "created_by", nullable = false, length = 50, updatable = false)
+//    @JsonIgnore
+//    private String createdBy;
+//
+//    @LastModifiedBy
+//    @Column(name = "last_modified_by", length = 50)
+//    @JsonIgnore
+//    private String lastModifiedBy;
 
     // Getters and Setters (Omitted for brevity)
 }
