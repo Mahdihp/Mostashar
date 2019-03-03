d3e765b229e0ffa7429016dd7d93af8796ba0ddf
9531404 <9531404@ceit.local>
2019-03-03 20:20:22 +0330

    new idea

--- src/main/java/ir/mostashar/model/rightMessage/repository/RightMessageRepo.java
+++ src/main/java/ir/mostashar/model/rightMessage/repository/RightMessageRepo.java
@@ -3,7 +3,7 @@ package ir.mostashar.model.rightMessage.repository;
 import ir.mostashar.model.rightMessage.RightMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import sun.plugin.util.UIUtil;
+//import sun.plugin.util.UIUtil;
 
 import java.util.Optional;
 import java.util.UUID;
