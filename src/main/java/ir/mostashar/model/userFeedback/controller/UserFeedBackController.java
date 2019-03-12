c359a081bfd478ce30fedbf34572495594066d5a
mahdi <mahdihp.devsc@gmail.com>
2019-03-12 16:03:00 +0330

    Change & Clean Code v8.

--- src/main/java/ir/mostashar/model/userFeedback/controller/UserFeedBackController.java
+++ src/main/java/ir/mostashar/model/userFeedback/controller/UserFeedBackController.java
@@ -3,17 +3,13 @@ package ir.mostashar.model.userFeedback.controller;
 
 import io.swagger.annotations.ApiOperation;
 import ir.mostashar.model.BaseDTO;
-import ir.mostashar.model.userFeedback.dto.UserFeedBackForm;
 import ir.mostashar.model.userFeedback.service.UserFeedBackService;
-import ir.mostashar.model.wallet.dto.WalletForm;
 import ir.mostashar.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -23,12 +19,18 @@ public class UserFeedBackController {
     @Autowired
     UserFeedBackService ufbService;
 
-    @ApiOperation(value = "Create User FeedBack", notes = "RequestBody :" + MediaType.APPLICATION_JSON_UTF8_VALUE)
+    /**
+     * Add FeedBack client From Lawyer By Client
+     *
+     * @param clientId
+     * @param description
+     * @param score
+     * @return
+     */
+    @ApiOperation(value = "Create User FeedBack")
     @PostMapping(value = "/create", consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE}, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<?> createFeedBack(@Valid @RequestBody UserFeedBackForm udbForm) {
-        if (ufbService.createUserFeedBack(udbForm))
-            return ResponseEntity.status(HttpStatus.OK).body(new BaseDTO(HttpStatus.OK.value(), Constants.KEY_SUCESSE));
-        else
-            return ResponseEntity.status(HttpStatus.OK).body(new BaseDTO(HttpStatus.OK.value(), Constants.KEY_FAIL));
+    public ResponseEntity<?> createFeedBack(@RequestParam("clientid") String clientId, @RequestParam("description") String description, @RequestParam("score") int score) {
+        ufbService.create(clientId, description, score);
+        return ResponseEntity.status(HttpStatus.OK).body(new BaseDTO(HttpStatus.OK.value(), Constants.KEY_SUCESSE));
     }
 }
